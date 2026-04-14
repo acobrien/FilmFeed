0705fb63a3a0656c540d8386000678666eb9b612
Aidan O'Brien <aobrien5750@gmail.com>
2026-04-14 07:46:23 +0200

    a work of art
    it's beautiful now

--- app/src/main/java/com/example/filmfeed/DetailsPage.kt
+++ app/src/main/java/com/example/filmfeed/DetailsPage.kt
@@ -2,7 +2,11 @@ package com.example.filmfeed
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -11,9 +15,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
 import com.example.filmfeed.models.Movie
 import com.example.filmfeed.ui.theme.FilmFeedTheme
@@ -23,7 +29,9 @@ import com.example.filmfeed.utils.Constants
 @Composable
 fun DetailsPage(movie: Movie, modifier: Modifier = Modifier) {
     Column(
-        modifier = modifier,
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         AsyncImage(
@@ -36,23 +44,36 @@ fun DetailsPage(movie: Movie, modifier: Modifier = Modifier) {
                 .height(138.dp),
             contentScale = ContentScale.Crop
         )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = movie.title,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.SemiBold
+        )
+        Spacer(modifier = Modifier.height(8.dp))
         Text(
             text = "${movie.genres}"
         )
-        val uriHandler = LocalUriHandler.current
+        Spacer(modifier = Modifier.height(8.dp))
 
-        Text(
-            text = movie.homepageUrl,
-            color = Color.Blue,
-            textDecoration = TextDecoration.Underline,
-            modifier = Modifier.clickable { uriHandler.openUri(movie.homepageUrl) }
-        )
-        Text(
-            text = movie.getImdbUrl(),
-            color = Color.Blue,
-            textDecoration = TextDecoration.Underline,
-            modifier = Modifier.clickable { uriHandler.openUri(movie.getImdbUrl()) }
-        )
+        Row {
+            val uriHandler = LocalUriHandler.current
+            Text(
+                text = "Movie Homepage",
+                color = Color.Blue,
+                textDecoration = TextDecoration.Underline,
+                modifier = Modifier.clickable { uriHandler.openUri(movie.homepageUrl) }
+            )
+            Text(
+                text = " | "
+            )
+            Text(
+                text = "IMDB Page",
+                color = Color.Blue,
+                textDecoration = TextDecoration.Underline,
+                modifier = Modifier.clickable { uriHandler.openUri(movie.getImdbUrl()) }
+            )
+        }
     }
 }
 
--- app/src/main/java/com/example/filmfeed/InfoPage.kt
+++ app/src/main/java/com/example/filmfeed/InfoPage.kt
@@ -1,22 +1,49 @@
 package com.example.filmfeed
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.filmfeed.ui.theme.FilmFeedTheme
 
 @Composable
 fun InfoPage(modifier: Modifier = Modifier) {
     Column(
-        modifier = modifier,
+        modifier = modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+            .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = "https://github.com/acobrien"
+        Image(
+            painterResource(R.drawable.github_lockup_black_clearspace),
+            contentDescription = "GitHub logo"
         )
+        Spacer(modifier = Modifier.height(16.dp))
+        val uriHandler = LocalUriHandler.current
+        Text(
+            text = "https://github.com/acobrien/FilmFeed",
+            color = Color.Blue,
+            textDecoration = TextDecoration.Underline,
+            modifier = Modifier.clickable {
+                uriHandler.openUri("https://github.com/acobrien/FilmFeed")
+            }
+        )
+        Spacer(modifier = Modifier.height(120.dp))
     }
 }
 
